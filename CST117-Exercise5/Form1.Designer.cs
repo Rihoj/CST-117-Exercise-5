3acb2606d6ba6b5e8317a5b559ef851a8fb55b94
Rihoj <rihoj1@gmail.com>
2018-05-13 23:00:59 -0400

    This is my work

--- CST117-Exercise5/Form1.Designer.cs
+++ CST117-Exercise5/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CST117_Exercise5
 {
-    partial class Form1
+    partial class calcPiForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.termsLabel = new System.Windows.Forms.Label();
+            this.termsTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultTitleLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Approximate PI";
+            // 
+            // termsLabel
+            // 
+            this.termsLabel.AutoSize = true;
+            this.termsLabel.Location = new System.Drawing.Point(12, 71);
+            this.termsLabel.Name = "termsLabel";
+            this.termsLabel.Size = new System.Drawing.Size(85, 13);
+            this.termsLabel.TabIndex = 1;
+            this.termsLabel.Text = "Enter # of terms:";
+            // 
+            // termsTextBox
+            // 
+            this.termsTextBox.Location = new System.Drawing.Point(104, 71);
+            this.termsTextBox.Name = "termsTextBox";
+            this.termsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.termsTextBox.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(12, 123);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(192, 59);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resultTitleLabel
+            // 
+            this.resultTitleLabel.AutoSize = true;
+            this.resultTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultTitleLabel.Location = new System.Drawing.Point(9, 208);
+            this.resultTitleLabel.Name = "resultTitleLabel";
+            this.resultTitleLabel.Size = new System.Drawing.Size(0, 16);
+            this.resultTitleLabel.TabIndex = 4;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(9, 236);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 5;
+            // 
+            // calcPiForm
+            // 
+            this.AcceptButton = this.calculateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(579, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.resultTitleLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.termsTextBox);
+            this.Controls.Add(this.termsLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "calcPiForm";
+            this.Text = "Calculate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label termsLabel;
+        private System.Windows.Forms.TextBox termsTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label resultTitleLabel;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
 
--- CST117-Exercise5/Form1.cs
+++ CST117-Exercise5/Form1.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace CST117_Exercise5
 {
-    public partial class Form1 : Form
+    public partial class calcPiForm : Form
     {
-        public Form1()
+        public calcPiForm()
         {
             InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int terms;
+            bool result = Int32.TryParse(termsTextBox.Text, out terms);
+            if (result)
+            {
+                decimal pi = 0;
+                decimal denominator = 1;
+                for(int i = 1; i <= terms; i++)
+                {
+                    if(i % 2 != 0)
+                    {
+                        pi = pi + (4 / denominator);
+                    }else
+                    {
+                        pi = pi - (4 / denominator);
+                    }
+                    denominator += 2;
+                }
+                resultTitleLabel.Text = "Approximate value of PI after " + terms + " terms";
+                resultLabel.Text = "= " + pi;
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer.", "Terms Error");
+            }
         }
     }
 }
